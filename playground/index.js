1925a5f6b0b2dd612e6bab67de66261b34d4a78e
hungbui7690 <hungbui7690@gmail.com>
2023-04-03 18:59:01 +0700

    Redis Keys and Values

--- playground/index.js
+++ playground/index.js
@@ -1,4 +1,16 @@
 /*
-  
+  Redis Keys and Values
+  - pic
 
+
+  - set name "Adnan"
+  - set fname "Adnan"
+  - set lname "Waheed"
+
+  - get name
+  - get fname
+  - get lname
+  - get lastname
+  - get "Adnan"
+  
 */
