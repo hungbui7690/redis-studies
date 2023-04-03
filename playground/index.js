ad37b5cfe4ead4aba059b5357d697e7cf33b41ca
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 05:35:47 +0700

    How Redis handle keys expirations

--- playground/index.js
+++ playground/index.js
@@ -1,14 +1,5 @@
 /*
-  How to remove expiration from a key
-  - pic
-
-  - set 1 hello ex 120
-  - ttl 1
-  - persist 1
-  - ttl 1
-  - set 2 hello
-  - persist 2
-  - set hello ex 10
-  - ttl 3
+  How Redis handle keys expirations
+  - pic only
 
 */
