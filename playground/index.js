01cc6d757316260ad196b3ac0a1fd9e3425c3643
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 05:48:56 +0700

    Key Spaces

--- playground/index.js
+++ playground/index.js
@@ -1,5 +1,30 @@
 /*
-  How Redis handle keys expirations
+  Key Spaces
   - pic only
 
+
+  - set key1 value1
+  - get key1
+  - set key1 value2
+  - get key1
+  - select 1
+  - set key1 value2
+  - get key1
+  - select 0
+  - get key1
+  - set key1 value1
+  - get key1
+  - keys *
+  - set key2 value2
+  - keys *
+  - select 0
+  - keys *
+  - flushdb
+  - keys *
+  - select 0
+  - flushdb
+  - keys *
+  - select 1
+  - select 2
+  
 */
