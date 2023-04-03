a3e791d61a66bddabbb837af99bd5da8d201c13d
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 06:58:09 +0700

    Keys naming conventions

--- playground/index.js
+++ playground/index.js
@@ -1,30 +1,18 @@
 /*
-  Key Spaces
-  - pic only
+  Keys naming conventions
+  - pic
+
+  - set userAdam Adam
+  - set userAmy Amy
+  - keys *
+  - set user:100 Adam
+  - set user:101 Amy
+  - keys *
+  - del userJohn userAdam userAmy
+  - keys *
+  - set user:100:friends Scott
+  - set user:101:friends John
+  - keys *
 
 
-  - set key1 value1
-  - get key1
-  - set key1 value2
-  - get key1
-  - select 1
-  - set key1 value2
-  - get key1
-  - select 0
-  - get key1
-  - set key1 value1
-  - get key1
-  - keys *
-  - set key2 value2
-  - keys *
-  - select 0
-  - keys *
-  - flushdb
-  - keys *
-  - select 0
-  - flushdb
-  - keys *
-  - select 1
-  - select 2
-  
 */
