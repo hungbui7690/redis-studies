057730b3faead0f38f0a7667ba399e379b2a8a54
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 05:33:46 +0700

    Remove expiration from a key

--- playground/index.js
+++ playground/index.js
@@ -1,30 +1,14 @@
 /*
-  How to define keys with expiration
+  How to remove expiration from a key
   - pic
 
   - set 1 hello ex 120
-    > key = 1, value = hello, expire = 120s
-
   - ttl 1
-    > ttl <key_name> > time to live > show how many seconds key will be expired
-
-
-  - expire 1 10
-    > change the expire time of a key
-    > expire <key> <time>
-      > expire key 1 in 10s 
-
-
-  - set 1 hello px 1000
-    > px: time in ms 
-
-
-  - pttl 1
-    > similar to ttl > but return ms 
-
-
-  - pexpire 1 100
-    > set key 1 to expire in 100ms
-
+  - persist 1
+  - ttl 1
+  - set 2 hello
+  - persist 2
+  - set hello ex 10
+  - ttl 3
 
 */
