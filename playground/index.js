546d37ab88be8b7b000da8f6154a0f04bd7ee51b
hungbui7690 <hungbui7690@gmail.com>
2023-04-03 21:00:26 +0700

    How to define keys with expiration

--- playground/index.js
+++ playground/index.js
@@ -1,10 +1,30 @@
 /*
-  How to check if a key exists or not
+  How to define keys with expiration
   - pic
 
-  - exists fname
-  - exists lname
-  - exists fname lname
-  - exists name
+  - set 1 hello ex 120
+    > key = 1, value = hello, expire = 120s
+
+  - ttl 1
+    > ttl <key_name> > time to live > show how many seconds key will be expired
+
+
+  - expire 1 10
+    > change the expire time of a key
+    > expire <key> <time>
+      > expire key 1 in 10s 
+
+
+  - set 1 hello px 1000
+    > px: time in ms 
+
+
+  - pttl 1
+    > similar to ttl > but return ms 
+
+
+  - pexpire 1 100
+    > set key 1 to expire in 100ms
+
 
 */
