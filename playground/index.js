3fc97a386a417725e23a493e2ad252323a31fe73
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 07:39:29 +0700

    Deleting Keys Asynchronously via UNLINK

--- playground/index.js
+++ playground/index.js
@@ -1,17 +1,6 @@
 /*
-  Rename a key with caution using RENAMENX
+  Deleting Keys Asynchronously via UNLINK
   - pic
-
-  - set k1 v1
-  - set k2 v2
-  - get k1
-  - get k2
-  - renamenx k1 k2
-  - get k1
-  - get k2
-  - renamenx k1 k3
-  - get k1
-  - get k3
 
 
 */
