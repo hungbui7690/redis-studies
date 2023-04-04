a091b7181b259c5bbccc020bf5efbaa0d5365eb0
hungbui7690 <hungbui7690@gmail.com>
2023-04-04 07:35:26 +0700

    Rename a key with caution using RENAMENX

--- playground/index.js
+++ playground/index.js
@@ -1,24 +1,17 @@
 /*
-  Rename a Key - RENAME
+  Rename a key with caution using RENAMENX
   - pic
 
-  - set name1 "John"
-  - set name2 "Doe"
-  - get name1
-  - get name2
-  - rename name1 fname
-  - get name1
-  - get fname
-  - rename name2 lname
-  - get name2
-  - get lname
-  - rename 11 abc
-  - rename fname lastname
-  - get fname
-  - get lname
-  - rename fname lname
-  - get fname
-  - get lname
+  - set k1 v1
+  - set k2 v2
+  - get k1
+  - get k2
+  - renamenx k1 k2
+  - get k1
+  - get k2
+  - renamenx k1 k3
+  - get k1
+  - get k3
 
 
 */
